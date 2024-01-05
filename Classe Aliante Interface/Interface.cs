7d4680c8d354c56e2dfad0883996137b3ee417c4
Thomas Rota <rota.thomas.studente@itispaleocapa.it>
2024-01-05 15:43:29 +0100

    <creazione interface>

---
+++ Classe Aliante Interface/Interface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Aliante_Interface
+{
+	public interface IComposite
+	{
+		void Aggiunta(IComposite componente);
+
+		void Rimuovi(int indice);
+
+		IComposite GetChild(int indice);
+
+		bool Equals(object obj);
+
+		string ToString();
+
+		double Prezzo();
+	}
+}
